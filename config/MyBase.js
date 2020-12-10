d17d68b92269dfdf663bb73b69c4f183651b59f2
곽진우 <jinookawk@gwagjin-uui-MacBookPro.local>
2020-12-10 16:35:24 +0900

    change firebase config to env file

--- config/MyBase.js
+++ config/MyBase.js
@@ -1,15 +1,25 @@
 import firebase from "firebase";
 import "firebase/firestore";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNRrX_KHSjaEIzkC0gYE-qLyiMs6XWHBo",
-  authDomain: "test-app-kawk.firebaseapp.com",
-  databaseURL: "https://test-app-kawk.firebaseio.com",
-  projectId: "test-app-kawk",
-  storageBucket: "test-app-kawk.appspot.com",
-  messagingSenderId: "447709582973",
-  appId: "1:447709582973:web:b4339a86b175091c234314",
-  measurementId: "G-Y6MRF0JZMK",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 const myBase = firebase.initializeApp(firebaseConfig);
