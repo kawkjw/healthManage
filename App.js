6caa966727004b34c0f4e17c7e57b81f4bc6f093
kawkjw <tkfkdnwjd@gmail.com>
2021-03-12 18:31:23 +0900

    add expo update

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Auth from "./screens/Auth";
 import "./config/fixtimer";
 import * as Notifications from "expo-notifications";
-import { Text, TextInput } from "react-native";
+import { SafeAreaView, Text, TextInput } from "react-native";
+import * as Updates from "expo-updates";
+import { TextSize } from "./css/MyStyles";
 
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
@@ -17,5 +19,30 @@ export default function App() {
     Text.defaultProps.allowFontScaling = false;
     TextInput.defaultProps = TextInput.defaultProps || {};
     TextInput.defaultProps.allowFontScaling = false;
-    return <Auth />;
+
+    const [showUpdates, setShowUpdates] = useState(false);
+    useEffect(() => {
+        Updates.checkForUpdateAsync().then((update) => {
+            if (update.isAvailable) {
+                setShowUpdates(true);
+                Updates.fetchUpdateAsync().then(async () => {
+                    await Updates.reloadAsync();
+                });
+            }
+        });
+    }, []);
+
+    return (
+        <>
+            {showUpdates ? (
+                <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                    <Text style={TextSize.largerSize}>업데이트 중 입니다.</Text>
+                    <Text style={TextSize.largerSize}>업데이트 완료 후 어플이 재실행되니</Text>
+                    <Text style={TextSize.largerSize}>잠시만 기다려주세요.</Text>
+                </SafeAreaView>
+            ) : (
+                <Auth />
+            )}
+        </>
+    );
 }
