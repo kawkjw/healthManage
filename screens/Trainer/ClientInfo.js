53292b46e88ede31b71a3c17d5689e0f1f633496
kawkjw <tkfkdnwjd@gmail.com>
2021-04-21 18:09:28 +0900

    add loading in trainer ClientInfo.js

--- screens/Trainer/ClientInfo.js
+++ screens/Trainer/ClientInfo.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Linking, ScrollView, Text, TouchableOpacity, View } from "react-native";
-import { Surface } from "react-native-paper";
+import { ActivityIndicator, Surface } from "react-native-paper";
 import { priceToString } from "../../config/hooks";
 import myBase, { db } from "../../config/MyBase";
 import { MyStyles, TextSize, theme } from "../../css/MyStyles";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import moment from "moment";
 
 export default ClientInfo = ({ navigation, route }) => {
     if (!route.params) {
@@ -12,8 +13,10 @@ export default ClientInfo = ({ navigation, route }) => {
     }
     const ptName = route.params.className.split(".")[0] === "pt" ? "pt" : "squashpt";
     const [clientsInfo, setClientsInfo] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const getMyClients = async () => {
+        setLoading(true);
         await db
             .collectionGroup("memberships")
             .where("classes", "array-contains", ptName)
@@ -67,49 +70,70 @@ export default ClientInfo = ({ navigation, route }) => {
     };
 
     useEffect(() => {
-        getMyClients();
+        getMyClients().then(() => {
+            setLoading(false);
+        });
     }, []);
 
     return (
         <View style={{ flex: 1, paddingTop: 10 }}>
-            <ScrollView>
-                {clientsInfo.length === 0 ? (
-                    <Text>No clients</Text>
-                ) : (
-                    clientsInfo.map((client, idx) => (
-                        <Surface style={[MyStyles.surface, { marginHorizontal: 20 }]} key={idx}>
-                            <View style={{ padding: 10 }}>
-                                <Text style={TextSize.normalSize}>
-                                    이름 : {client.userInfo.name}
-                                </Text>
-                                <View style={{ flexDirection: "row" }}>
-                                    <Text style={TextSize.normalSize}>휴대폰번호 : </Text>
-                                    <TouchableOpacity
-                                        onPress={() =>
-                                            Linking.openURL(`tel:${client.userInfo.phoneNumber}`)
-                                        }
-                                    >
-                                        <Text
-                                            style={[TextSize.normalSize, { color: "dodgerblue" }]}
+            {loading ? (
+                <View style={{ flex: 1, justifyContent: "center" }}>
+                    <ActivityIndicator size="large" color="black" animating={true} />
+                </View>
+            ) : (
+                <ScrollView>
+                    {clientsInfo.length === 0 ? (
+                        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                            <Text style={TextSize.largeSize}>담당 고객이 없습니다.</Text>
+                        </View>
+                    ) : (
+                        clientsInfo.map((client, idx) => (
+                            <Surface style={[MyStyles.surface, { marginHorizontal: 20 }]} key={idx}>
+                                <View style={{ padding: 10 }}>
+                                    <Text style={TextSize.normalSize}>
+                                        이름 : {client.userInfo.name}
+                                    </Text>
+                                    <View style={{ flexDirection: "row" }}>
+                                        <Text style={TextSize.normalSize}>휴대폰번호 : </Text>
+                                        <TouchableOpacity
+                                            onPress={() =>
+                                                Linking.openURL(
+                                                    `tel:${client.userInfo.phoneNumber}`
+                                                )
+                                            }
                                         >
-                                            {client.userInfo.phoneNumber}
-                                        </Text>
-                                    </TouchableOpacity>
+                                            <Text
+                                                style={[
+                                                    TextSize.normalSize,
+                                                    { color: "dodgerblue" },
+                                                ]}
+                                            >
+                                                {client.userInfo.phoneNumber}
+                                            </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                    <Text style={TextSize.normalSize}>
+                                        등록일 :{" "}
+                                        {moment(client.ptInfo.start.toDate()).format(
+                                            "YYYY년 M월 D일"
+                                        )}
+                                    </Text>
+                                    <Text style={TextSize.normalSize}>
+                                        그룹 PT : {client.ptInfo.group ? "O" : "X"}
+                                    </Text>
+                                    <Text style={TextSize.normalSize}>
+                                        남은 횟수 : {client.ptInfo.count}
+                                    </Text>
+                                    <Text style={TextSize.normalSize}>
+                                        결제 금액 : {priceToString(client.ptInfo.price)}원
+                                    </Text>
                                 </View>
-                                <Text style={TextSize.normalSize}>
-                                    그룹 PT : {client.ptInfo.group ? "O" : "X"}
-                                </Text>
-                                <Text style={TextSize.normalSize}>
-                                    남은 횟수 : {client.ptInfo.count}
-                                </Text>
-                                <Text style={TextSize.normalSize}>
-                                    결제 금액 : {priceToString(client.ptInfo.price)}원
-                                </Text>
-                            </View>
-                        </Surface>
-                    ))
-                )}
-            </ScrollView>
+                            </Surface>
+                        ))
+                    )}
+                </ScrollView>
+            )}
             <View
                 style={{ backgroundColor: theme.colors.primary, height: hp("6%"), width: "100%" }}
             />
